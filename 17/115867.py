8f15bf7b3d7a2acf10e8d69ce7a6d60bd80779fb
sm1gy <ignat_smotr@mail.ru>
2022-12-04 21:30:13 +0300

    04/12/2022 - 115867

--- 17/115867.py
+++ 17/115867.py
@@ -4,10 +4,10 @@ max_summ = 0
 with open('115867.txt') as txt:
     nums = list(map(int, txt.readlines()))
     for i in range(len(nums)):
-        if nums[i] > max_num and nums[i] % 10 == 3:
+        if nums[i] > max_num and abs(nums[i]) % 10 == 3:
             max_num = nums[i]
     for i in range(len(nums) - 1):
-        if ((nums[i] % 10 == 3 and nums[i+1] % 10 != 3) or (nums[i+1] % 10 == 3 and nums[i] % 10 != 3)) and (nums[i] ** 2 + nums[i+1] ** 2 >= max_num ** 2):
+        if ((abs(nums[i]) % 10 == 3 and abs(nums[i+1]) % 10 != 3) or (abs(nums[i+1]) % 10 == 3 and abs(nums[i]) % 10 != 3)) and (nums[i] ** 2 + nums[i+1] ** 2 >= max_num ** 2):
             s += 1
             max_summ = max(max_summ, nums[i] ** 2 + nums[i + 1] ** 2)
     print(s, max_summ)
