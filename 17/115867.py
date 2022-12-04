21bcf412b195a15125bb544f98236bf74659ff1e
sm1gy <ignat_smotr@mail.ru>
2022-12-04 20:45:19 +0300

    20/11/2022 - 9, 14, 15, 16

---
+++ 17/115867.py
@@ -0,0 +1,13 @@
+s = 0
+max_num = -10001
+max_summ = 0
+with open('115867.txt') as txt:
+    nums = list(map(int, txt.readlines()))
+    for i in range(len(nums)):
+        if nums[i] > max_num and nums[i] % 10 == 3:
+            max_num = nums[i]
+    for i in range(len(nums) - 1):
+        if ((nums[i] % 10 == 3 and nums[i+1] % 10 != 3) or (nums[i+1] % 10 == 3 and nums[i] % 10 != 3)) and (nums[i] ** 2 + nums[i+1] ** 2 >= max_num ** 2):
+            s += 1
+            max_summ = max(max_summ, nums[i] ** 2 + nums[i + 1] ** 2)
+    print(s, max_summ)
